e616eac540ffeddfb4fb98ae5c4e9b66ccc895fb
borisofzerty <fabiano.taddei@gmail.com>
2013-12-28 21:37:16 +0100

    new class design

--- shiftsub.rb
+++ shiftsub.rb
@@ -1,90 +1,70 @@
 require 'optparse'
 
-def parse
-  options = {}
+class Shifter
+  attr_accessor :in_p, :out_p, :time_f
+  # regexp for valid sub-timing line $1 => 1st time, $2 => 2nd time
+  GL_RE = /(\d\d:\d\d:\d\d,\d{3}) --> (\d\d:\d\d:\d\d,\d{3})/
 
-  opt_parser = OptionParser.new do |opt|
-    opt.banner = "Usage: #{$0} ADD|SUB [-t TIME]"
+  # TODO line_regexp - transofrm in commented regexp
+  # $1 hours 1st time, $2 min 1st time,...
+  # $5 .. $8 2nd time values
+  #              $1     $2     $3     $4          $5     $6     $7     $8
+  FULL_RE = /\A(\d\d):(\d\d):(\d\d),(\d{3}) --> (\d\d):(\d\d):(\d\d),(\d{3})/
 
-    opt.on("-t","--time TIME","time to add/substract from subtitles") do |time|
-      # TODO: check if time is valid
-      options[:time] = (time.to_f * 1000).to_i
+  # will parse options, and fill @time_f, @f_in and @f_out variables
+  # will print error message if wrong parameters are given
+  def initialize(shift_time, in_p = nil, out_p = nil)
+    # TODO check args
+    @in_p, @out_p, @shift_time = in_p, out_p, shift_time
+  end
+
+  def openfiles
+    if @in_p
+      @f_in = File::open(@in_p, "r")
+      if @out_p
+        @f_out = File::open(@out_p, "w")
+      else
+        @f_out = $stdout
+      end
+    else
+      @f_in  = $stdin
+      @f_out = $stdout
     end
+  end
 
-    opt.on("-h","--help","help") do
-      puts opt_parser
+  def convert
+    openfiles
+    @f_in.each_line do |line|
+      if line =~ FULL_RE
+        #TODO use array to avoid writing same code 2 times
+        t1 = Time::mktime(0, 1, 1, $1.to_i, $2.to_i, $3.to_i, $4.to_i * 1000)
+        t1 += @shift_time
+        t1 = t1.strftime("%H:%M:%S,%L")
+
+        t2 = Time::mktime(0, 1, 1, $5.to_i, $6.to_i, $7.to_i, $8.to_i * 1000)
+        t2 += @shift_time
+        t2 = t2.strftime("%H:%M:%S,%L")
+        puts t1 + " --> " + t2
+      else
+        @f_out.write(line)
+      end
     end
   end
-
-  opt_parser.parse!
-
-  unless ARGV[0].is_a?(String)
-    puts opt_parser
-    exit 1
-  end
-
-  case ARGV[0]
-  when /ADD/
-    options[:time] *= 1
-  when /SUB/
-    options[:time] *= -1
-  else
-    puts opt_parser
-    exit 1
-  end
-
-  return options
 end
 
-# convert millisecond in string
-# ms: (Fixnum)
-# RETURN: (String) in format hh:mm:ss,mmmm
-def ms_to_st(ms)
-  milli = ms % 1000
-  ms -= milli
-  ms /= 1000
-
-  sec = ms % 60
-  ms /= 60
-
-  min = ms % 60
-  ms /= 60
-
-  hours = ms
-
-  return sprintf("%02d:%02d:%02d,%03d", hours, min, sec, milli)
-
+# is first arg is "+" or "-" just remove it and remember the sign
+if ARGV[0] =~ /\A([+-])\z/
+  sign = $1
+  ARGV.shift
 end
 
-# convert string in milliseconds
-# st: (String) in format hh:mm:ss,mmmm
-# RETURN: (Fixnum) amount of milisecond
-def st_to_ms(st)
-  hmsm_re = /(\d\d):(\d\d):(\d\d),(\d{3})/
-  st.match(hmsm_re)
-  return ($1.to_i * 3600 + $2.to_i * 60 + $3.to_i) * 1000 + $4.to_i
+unless ARGV[0] =~ /\A([+-]?[0-9.]+)(ms?|s?)\z/
+  $stderr.puts "wrong argument"
+  exit 1
 end
+time_f = $1.to_f
+time_f /= 1000 unless $2[0] == "s"
+time_f *= -1 if sign =~ /-/
 
-# add or substract time from a string
-# original: (String) time in the format hh:mm:ss,mmm
-# delay: (Fixnum) time to add in milliseconds
-# RETURN: (String) updated time string
-def change_time(original, delay)
-  original = st_to_ms(original)
-  return ms_to_st(original + delay)
-end
-
-options = parse
-
-# TODO: multiline match
-# TODO: put this into a function
-gl_re = /(\d\d:\d\d:\d\d,\d{3}) --> (\d\d:\d\d:\d\d,\d{3})/
-$stdin.each_line do |line|
-  if line =~ gl_re
-    ut_1 = (st_to_ms($1) + options[:time])
-    ut_2 = (st_to_ms($2) + options[:time])
-    puts ms_to_st(ut_1) + " --> " + ms_to_st(ut_2)
-  else
-    puts line
-  end
-end
+s = Shifter::new(time_f,ARGV[1],ARGV[2])
+s.convert
